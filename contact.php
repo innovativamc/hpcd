50368bfae3894a9596e8509545a453f2cc873f0b
Jose Mujica <josemujica@webhorse.dlbgroup.local>
2013-04-30 17:06:52 -0430

    moví artes folder al directorio principal

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="css/style.css">  
+<link rel="stylesheet" href="FlexSlider-Home/flexslider.css" type="text/css" media="screen" />
+  <script src="js/prefixfree.min.js"></script>
+  <script src="js/jquery-1.9.1.min.js"></script>	
+  <script src="js/functions.js"></script>
+
+
+<title>HPCD</title>
+</head>
+<body>
+<?php include('box/header.php') ?>	
+<section class="contenedor">	 
+</section>	
+<?php $galeria=null; include('box/footer.php') ?>
+</body>
+</html>
