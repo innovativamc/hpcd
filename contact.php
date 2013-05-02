46a87b1ab056020469654cb4d8423dce87f7664d
Jose Mujica <josemujica@webhorse.dlbgroup.local>
2013-05-02 16:40:17 -0430

    agregue contact.php y option3.php, style.css actualiado

--- contact.php
+++ contact.php
@@ -4,6 +4,11 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" href="css/style.css">  
 <link rel="stylesheet" href="FlexSlider-Home/flexslider.css" type="text/css" media="screen" />
+<style>
+	body{ 
+		background:url( ); 
+	}
+</style>
   <script src="js/prefixfree.min.js"></script>
   <script src="js/jquery-1.9.1.min.js"></script>	
   <script src="js/functions.js"></script>
@@ -14,6 +19,55 @@
 <body>
 <?php include('box/header.php') ?>	
 <section class="contenedor">	 
+	<section class="option3top">
+		<img src="images/option3.png" width="100%" alt="">
+	</section>
+	<section class="option3left">
+		<figure><img src="images/option3_2.png" width="46%" alt=""/> <img src="images/option3_3.png" width="46%" alt=""/> </figure>
+		<figure><img src="images/option3_4.png" width="95%" alt=""/> </figure>
+	</section>
+	<section class="option3right">
+		<h4>CONTACTO</h4> 
+		<div>
+			<h4>OFICINA PRINCIPAL</h2>
+			<div>
+				<strong>Corporativo:</strong> Edificio Atrium, piso 3, Av. Venezuela c/c Sorocaima. Urb. El
+				Rosal,Caracas 1060,Venezuela. <br />
+				<strong>Propiedad Intelectual:</strong> Torre IASA, piso 3, Av. Eugenio Mendoza, Plaza La
+				Castellana,Caracas 1060,Venezuela.  <br />
+				TIf: +58 212 201-8611, +58 212 263-6744  <br />
+				Fax: +58 212 263-7744  <br />
+			</div>
+			<h4>dirección de correo internacional</h4>
+			<div>
+				Hoet Pelaez Castillo & Duque  <br />
+				CCS 13031  <br />
+				P.O. Box 025323  <br />
+				Miami, FL 33102-5323  <br />
+				USA  <br />	
+			</div>
+			<h4>dirección de correo en venezuela</h4>
+			<div>
+				Apartado 62.414, Caracas 1060-A, Venezuela <br />
+				<strong>Divisiones Corporativa y Administrativa</strong> <br />
+				InfoLaw@hpcd.com <br />
+				<strong>Contactos</strong> <br />
+				<strong>Socios</strong> <br />
+				Fernando Peláez: fpelaez@hpcd.com <br />
+				Francisco Castillo: fcastillo@hpcd.com <br />
+				Jorge Acedo Prato: jacedo@hpcd.com <br />
+				Luis López Duran: llopez@hpcd.com <br />
+				<strong>Administración y Finanzas </strong><br />
+				Minerva Ventura: mventura@hpcd.com <br />
+				<strong>Cobranzas</strong> <br />
+				Betty Venegas: bvenegas@hpcd.com <br />
+				<strong>Sistemas </strong><br />
+				Victor Lugo: vlugo@hpcd.com <br />
+				Gerencia y Tecnología Consultiva, C. A. <br />
+			</div>
+		</div>
+	</section>
+	<br /><br /><br /><br />
 </section>	
 <?php $galeria=null; include('box/footer.php') ?>
 </body>
---
+++ option3.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="css/style.css">  
+<link rel="stylesheet" href="FlexSlider-Home/flexslider.css" type="text/css" media="screen" />
+<style>
+	body{ 
+		background:url( ); 
+	}
+</style>
+  <script src="js/prefixfree.min.js"></script>
+  <script src="js/jquery-1.9.1.min.js"></script>	
+  <script src="js/functions.js"></script>
+
+
+<title>HPCD</title>
+</head>
+<body>
+<?php include('box/header.php') ?>	
+<section class="contenedor">	 
+	<section class="option3top">
+		<img src="images/option3.png" width="100%" alt="">
+	</section>
+	<section class="option3left">
+		<figure><img src="images/option3_2.png" width="46%" alt=""/> <img src="images/option3_3.png" width="46%" alt=""/> </figure>
+		<figure><img src="images/option3_4.png" width="95%" alt=""/> </figure>
+	</section>
+	<section class="option3right">
+		<p>
+		<h4>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur</h4>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</section>
+	<br /><br /><br />
+</section>	
+<?php $galeria=null; include('box/footer.php') ?>
+</body>
+</html>
