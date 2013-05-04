32f5e03ca21937edb6eff16de6a48bc05f5d869a
David Rojas <jondavid327@msn.com>
2013-05-03 19:44:14 -0430

    asignacion de areas practicas y de servicios

--- box/search_people.php
+++ box/search_people.php
@@ -8,18 +8,58 @@
 						<a href="people_select.php">R</a> | S | T | U | V | W | X | Y | Z | todos 
 					</h2>
 						<p>
-							<select>
+							<select maxlength="50">
 		  						<option value="0">Seleccione Sector</option>
-		  						<option value="0">Seleccione una Practica</option>
+		  						<option value="0" >Seleccione una Practica</option>
 							</select>
 						</p>
 					<p>
-						<select>
+						<select maxlength="50">
+
+						<?php 
+
+						if($_SESSION["profesionales"]=="abogados")
+
+						{ 
+
+							echo '	
 	  						<option value="0">Seleccione una Practica</option>
+	  						<option value="1">Contratos, Fusiones y Adquisiciones</option>
+	  						<option value="2">Energía, Recursos Naturales, Servicios Básicos y Concesiones</option>
+	  						<option value="3">Telecomunicaciones, Tecnología de la Información y Comercio Electrónico</option>
+	  						<option value="4">Comercio Internacional, Inversiones, Banca, Finanzas, Competencia, Régimen Control
+Cambiario</option>
+	  						<option value="5">Derecho Laboral y Seguridad Social</option>
+	  						<option value="6">Litigios y Medios Alternos de Solución de Controversias</option>
+
+	  						<option value="7">Inmobiliario</option>
+	  						<option value="8">Industria Farmacéutica / Salud</option>
+	  						<option value="9">Tributario</option>
+	  						<option value="10">Derecho Público</option> '; 
+	  					}
+	  					if($_SESSION["profesionales"]=="asesores")
+
+						{ 
+
+							echo '	
+	  						<option value="0">Seleccione un Servicio</option>
+	  						<option value="1">Asesoría General</option>
+	  						<option value="2">Patentes</option>
+	  						<option value="3">Marcas</option>
+	  						<option value="4">Derechos de Autor</option>
+	  						<option value="5">Análisis de Protección Múltiple</option>
+	  						<option value="6">Nombres de Dominio</option>
+							<option value="7">Registro Sanitario</option>
+	  						<option value="8">Cambios en Marcas y Patentes</option>
+	  						<option value="9">Licencias y Transferencia Tecnológica</option>
+	  						<option value="10">Protección contra Competencia Desleal</option>
+	  						<option value="11">Medios Alternos de Solución de Controversias y Litigios</option> '; 
+	  					}	?>
+
 						</select>
 					</p>
 					<p>
-						<select>
+						<select maxlength="50">
 	  						<option value="0">Seleccione Lugar</option>
 	  						<option value="0">Seleccione una Practica</option>
 						</select>
